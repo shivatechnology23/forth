6977d7d5868e76699e00a5a5cdcd9611a3531a8f
shivatechnology23 <shivatechnology23@gmail.com>
2024-05-23 20:43:50 +0530

    forth

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+}
